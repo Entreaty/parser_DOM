677e00c3995167154d7ece57d793d87d2d375ef2
Entreaty <Draiko@sibmail.com>
2015-03-23 22:03:13 +0600

    RID & ID is ready

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <pre>
 <?php
 /**get_links()*@author Jay Gilford @param string $url @return array  */
-function get_links($url) {
+function get_RID_links($url) {
 
     // Create a new DOM Document to hold our webpage structure
     $xml = new DOMDocument();
@@ -15,18 +15,44 @@ function get_links($url) {
 
     //Loop through each <a> tag in the dom and add it to the link array
     foreach($xml->getElementsByTagName('a') as $link) {
-        $links[] = array('url' => $link->getAttribute('href'), 'text' => trim($link->nodeValue));
+
+        if(preg_match('@.*?modules.php\\?name=docum_sud&rid=@',$link->getAttribute('href')))
+
+            $links[] = array('url' => $link->getAttribute('href'), 'text' => trim($link->nodeValue));
     }
 
     //Return the links
     return $links;
 }
-$links=@get_links("http://kirovsky.tms.sudrf.ru/modules.php?name=docum_sud&rid=14");
+function get_ID_links($url) {
 
+    // Create a new DOM Document to hold our webpage structure
+    $xml = new DOMDocument();
 
+    // Load the url's contents into the DOM
+    $xml->loadHTMLFile($url);
 
-for($i=0; $i<count($links); $i++){echo($i.'= '.$links[$i][url]).'<br>';}
-//foreach($links as $link){echo(array_search('modules.php?name=docum_sud&id=601',$link));}
-//print_r($links);
+    // Empty array to hold all links to return
+    $links = array();
+
+    //Loop through each <a> tag in the dom and add it to the link array
+    foreach($xml->getElementsByTagName('a') as $link) {
+
+        if(preg_match('@.*?modules.php\\?name=docum_sud&id=@',$link->getAttribute('href')))
+
+            $links[] = array('url' => $link->getAttribute('href'), 'text' => trim($link->nodeValue));
+    }
+
+    //Return the links
+    return $links;
+}
+
+$RIDs=@get_RID_links("http://kirovsky.tms.sudrf.ru/modules.php?name=docum_sud&rid=7");
+
+print_r($RIDs);
+
+$IDs=@get_ID_links("http://kirovsky.tms.sudrf.ru/modules.php?name=docum_sud&rid=14");
+
+print_r($IDs);
 ?>
 
